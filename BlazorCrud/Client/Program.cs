7944afa9c6e9ce1687d68560ecd8a6349fe8322c
Dan Patrascu-Baba <dan@danpatrascu.com>
2021-01-17 22:04:48 +0200

    Add authentication with Azure AD

--- BlazorCrud/Client/Program.cs
+++ BlazorCrud/Client/Program.cs
@@ -19,6 +19,12 @@ namespace BlazorCrud.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddMsalAuthentication(opt => 
+            {
+                builder.Configuration.Bind("AzureAd", opt.ProviderOptions.Authentication);
+                opt.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/openid");
+            });
+
             await builder.Build().RunAsync();
         }
     }
